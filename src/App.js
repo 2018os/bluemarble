8aeb74ffe9574a0a8c57f1046ce97ed3b1231210
2018os <tmdduq4536@naver.com>
2019-01-18 23:07:16 +0900

    player

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import Board from './board/Board';
+import Player from './player/Player';
 
 class App extends Component {
+  state = {
+    move: 0
+  }
+
+  handlePlayerMove = (e) => {
+    const { value } = e.target;
+    this.setState({
+      move: value
+    });
+  }
+
+
   render() {
     return (
-      <Board />
+      <div>
+        <Board />
+        <Player onMove={this.handlePlayerMove} />
+      </div>
     );
   }
 }
