2ec3eff1aabf4c754fc7f8bf53cea625d9631c90
Ramon <toby2009@naver.com>
2019-01-18 14:24:10 +0900

    맵 보여주기

--- src/board/CountryList.js
+++ src/board/CountryList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Country from './Country';
-import './Country.css';
+import './Country.scss';
 
 class CountryList extends Component {
   static defaultProps = {
@@ -15,7 +15,12 @@ class CountryList extends Component {
     console.log(list);
     return (
       <div className="CountryList">
+      <div className="country">
         {list}
+      </div>
+        <div className="random_button">
+          <button>누르기</button>
+        </div>
       </div>
     );
   }
