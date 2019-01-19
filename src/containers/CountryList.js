e6884504d05db14e3f1fe6fc5306224a1a42292b
Ramon <toby2009@naver.com>
2019-01-19 15:09:33 +0900

    Create CountryList.js

---
+++ src/containers/CountryList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Country from '../components/country/Country';
+import './Country.scss';
+import Counter from "../components/dice/dice";
+import CountryContainer from "./countrycontainer";
+
+class CountryList extends Component {
+  static defaultProps = {
+    data: []
+  };
+
+  render() {
+    const { data } = this.props;
+    const list = data.map(
+      info => (<Country key={info.id} info={info} />)
+    );
+    console.log(list);
+    return (
+      <div className="CountryList">
+        <div className="country">
+          {list}
+        </div>
+        <div className="random_button">
+          {/* <Counter /> */}
+          <CountryContainer />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CountryList;
