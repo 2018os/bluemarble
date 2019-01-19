2d20e407c49a7b7ad1fac4008b10fc9cea0d3a1d
Ramon <toby2009@naver.com>
2019-01-19 14:55:25 +0900

    Create index.js

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+import * as types from "../actions/actionTypes";
+
+const initialState = {
+    number: 0
+};
+
+function counter(state = initialState, action) {
+    switch(action.type) {
+        case types.Random:
+        return {
+            ...state,
+            number: action.number
+        };
+        default:
+        return state;
+    }    
+};
+
+export default counter;
