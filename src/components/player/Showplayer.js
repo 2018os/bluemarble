6d2989bbd901c6d0d98b3519fb28024b86ffb45b
STHyeon <toby2009@naver.com>
2019-01-22 15:01:48 +0900

    Create Showplayer.js

---
+++ src/components/player/Showplayer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import "./Showplayer.scss";
+
+class Showplayer extends Component {
+  render() {
+    return (
+      <div className="up_player">
+        <div className="A_player">
+            <p>id: </p>
+            <p>돈: </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Showplayer;
