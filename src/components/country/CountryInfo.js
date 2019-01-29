10bfcb0e89373fb5cf03a866a9ca0e428c6c6398
STHyeon <toby2009@naver.com>
2019-01-29 17:56:27 +0900

    Update CountryInfo.js

--- src/components/country/CountryInfo.js
+++ src/components/country/CountryInfo.js
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './CountryInfo.scss';
-import Modal from './modal';
+
+//jquery
+import * as $ from 'jquery';
+
+window.jQuery = window.$ = $
+//--------------------------
+
 
 class CountryInfo extends Component {
   render() {
     const { countries, player, onBuy, turn } = this.props;
     const { location } = player[turn];
     const { name, price, bought } = countries[location];
+
+    const ModalShow = () => {
+      $('#Country_detail').modal('show')
+    };
+
     return (
       <div className="countryInfo">
         <h1>{name}</h1>
         <h3>{price}원</h3>
         {
-          // !bought && location!==0 && <button onClick={onBuy}>구매 하시겠습니까?</button>
-          !bought && location!==0 && <Modal onBuy={onBuy} player={player} countries={countries} turn={turn}/>
+          !bought && location!==0 && ModalShow()
         }
+
+
+        <div className="modal fade" id="Country_detail" role="dialog">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">×</button>
+                <h4 className="modal-title">{name}</h4>
+              </div>
+              <div className="modal-body">
+                <p>땅 종류</p>
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-primary" onClick={onBuy} data-dismiss="modal">구매하기</button>
+                <button type="button" className="btn btn-default" data-dismiss="modal">닫기</button>
+              </div>
+            </div>
+            
+          </div>
+        </div>
       </div>
 
     )
