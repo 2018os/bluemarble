44d1d33e82e8762fdcd4c069ce0764bf0d308e22
Ramon <toby2009@naver.com>
2019-01-30 22:02:39 +0900

    Delete PlayerInfoContainer.js

--- src/containers/PlayerInfoContainer.js
+++
@@ -1,8 +0,0 @@
-import Player from "../components/player/Player";
-import { connect } from 'react-redux';
-
-//props 값으로 넣어줄 상태 정의
-const mapStateToProps = (state) => ({player: state.player, turn: state.turn});
-const PlayerInfoContainer = connect(mapStateToProps)(Player);
-
-export default PlayerInfoContainer;
