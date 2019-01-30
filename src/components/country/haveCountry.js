530ae3c5ce9183aa224297e2f0e8e61e660c4ed9
Ramon <toby2009@naver.com>
2019-01-30 22:02:09 +0900

    Create haveCountry.js

---
+++ src/components/country/haveCountry.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const HaveCountry = styled.div`
+${props => props.owner === 'player0' &&`
+    background-color: red;
+    width: 10px;
+    height: 10px;
+    `}
+
+    ${props => props.owner === 'player1' &&`
+    background-color: yellow;
+    width: 10px;
+    height: 10px;    
+    `}
+
+    ${props => props.owner === 'player2' &&`
+    background-color: green;
+    width: 10px;
+    height: 10px;    
+    `}
+
+    ${props => props.owner === 'player3' &&`
+    background-color: blue;
+    width: 10px;
+    height: 10px;    
+    `}
+`
+
+export default HaveCountry;
