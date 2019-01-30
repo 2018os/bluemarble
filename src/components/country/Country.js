6cc0c9ca1629dcefb734c0d477fd63daab7e9397
Ramon <toby2009@naver.com>
2019-01-30 22:00:53 +0900

    playerMove 헬프

--- src/components/country/Country.js
+++ src/components/country/Country.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import "./country_box.scss";
+import HaveCountry from "./haveCountry";
 import PropTypes from 'prop-types';
-import PlayerInfoContainer from "../../containers/PlayerInfoContainer";
+import Player1 from "../player/Player1";
+import Player2 from "../player/Player2";
+import Player3 from "../player/Player3";
+import Player4 from "../player/Player4";
 
-const Country = ({ name, price, done, owner }) => {   //done으로 player위치 확인
+const Country = ({ name, price, done, owner, turn, player }) => {   //done으로 player위치 확인
+  const turns = {turn};
+  // const turn1 = 1;
+  // const { playerMove } = player[turn];
+  console.log(player[turn].playerMove);
   return (
     <div className="country_nick">
       <div className="sub_detail">
         <div>{name}</div>
-        {owner}
-        {price}원
+        <HaveCountry owner={owner}></HaveCountry>
       </div>
-      { done && <PlayerInfoContainer />}
+      {/* { (done && playerMove) && <Player1 />} */}
+      {/* { (done && player[turn].id===1) && <Player2 />} */}
+      {/* { player[turn].playerMove && <div>aa</div>} */}
+      {/* {
+       done && (function() {
+         if (player[turn].id === 0) {
+          return (<Player1 />); 
+        } else if (turn === 1) {
+          return (<Player2 />);
+        } else if (turn === 2) {
+          return (<Player3 />);
+        } else if (turn === 3) {
+          return (<Player4 />);
+        } else {
+          return console.log("오류");
+        }
+        })()
+      } */}
     </div>
   );
 };
@@ -20,14 +44,18 @@ Country.propTypes = {
   name: PropTypes.string,
   price: PropTypes.number,
   done: PropTypes.bool,
-  owner: PropTypes.string
+  owner: PropTypes.string,
+  playerMove: PropTypes.bool,
+  turn: PropTypes.number
 };
 
 Country.defaultProps = {
   name: '',
   price: 0,
   done: false,
-  owner: ''
+  owner: '',
+  playerMove: false,
+  turn: 0,
 };
 
 export default Country;
