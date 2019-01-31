9c64635dc99c43be77d552606c83a81f7c4ec361
STHyeon <toby2009@naver.com>
2019-01-31 22:40:29 +0900

    Create Playerstyle.js

---
+++ src/components/player/Playerstyle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Playerturns = styled.div `
+    ${props=>props.userid === 0 &&`
+        background-color: red;
+    `}
+    ${props=>props.userid === 1 &&`
+        background-color: yellow;
+    `}
+    ${props=>props.userid === 2 &&`
+        background-color: green;
+    `}
+    ${props=>props.userid === 3 &&`
+        background-color: blue;
+    `}
+
+`
+
+export default Playerturns;
