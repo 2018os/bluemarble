03949915023cacf97bb07c0dcb0b7ad1daac807c
Ramon <toby2009@naver.com>
2019-02-02 00:06:56 +0900

    Update dice.js

--- src/components/dice/dice.js
+++ src/components/dice/dice.js
@@ -1,13 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import './dice.scss';
-// import Player from '../player/Player';
 
-const Dice = ({number, onRandom}) => {
+const Dice = ({number, senumber, onRandom}) => {
     return(
         <div className="random_button">
             <div className="random_dice_number">
-                주사위: {number}
+                주사위1: {number}
+                주사위2: {senumber}
             </div>
             <div>
                 <button onClick={onRandom} onContextMenu={(e)=> {
@@ -20,11 +20,13 @@ const Dice = ({number, onRandom}) => {
 
 Dice.propTypes = {
     number: PropTypes.number,
+    senumber: PropTypes.senumber,
     onRandom: PropTypes.func
 };
 
 Dice.defaultProps = {
     number: 0,
+    senumber: 0,
     onRandom: () => console.warn('onRandom not defined')
 };
 
