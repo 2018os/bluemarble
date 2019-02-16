96ea6e0fb6e2ef8bdef4de22c0b23f0b3f3d1364
STHyeon <toby2009@naver.com>
2019-02-16 15:07:30 +0900

    Update Playerstyle.js

--- src/components/player/Playerstyle.js
+++ src/components/player/Playerstyle.js
@@ -1,30 +1,45 @@
 import styled from "styled-components";
 
 const Playerstate = styled.div `
-    ${props=>props.location === 0 &&`
-        position: relative; 
-        bottom:13px;
+    ${props=>props.userid === 0 &&`
+    background-color: red;
     `}
-    ${props=>props.location > 0 && props.location <10 &&`
-    position: relative; 
-    bottom:49px;
+    ${props=>props.userid === 1 &&`
+    background-color: yellow;
     `}
-    ${props=>(props.location === 7 || props.location === 10 || props.location === 3) &&`
-        position: relative; 
-        bottom:17px;
+    ${props=>props.userid === 2 &&`
+    background-color: green;
     `}
-    ${props=>props.location >= 11 && props.location <=20 &&`
-        position: relative; 
-        bottom:49px;
+    ${props=>props.userid === 3 &&`
+    background-color: blue;
     `}
-    ${props=>props.location >= 21 && props.location <=30 &&`
-        position: relative; 
-        bottom:41px;
-    `}
-    ${props=>props.location >= 31 && props.location <=40 &&`
-        position: relative; 
-        bottom:38px;
-    `}
+
+    // ${props=>props.location === 0 &&`
+    //     background:red;
+    //     position: relative; 
+    //     bottom:13px;
+    // `}
+    // ${props=>props.location > 0 && props.location <10 &&`
+    //     background:yellow;
+    //     position: relative; 
+    //     bottom:49px;
+    // `}
+    // ${props=>(props.location === 7 || props.location === 10 || props.location === 3) &&`
+    //     position: relative; 
+    //     bottom:17px;
+    // `}
+    // ${props=>props.location >= 11 && props.location <=20 &&`
+    //     position: relative; 
+    //     bottom:49px;
+    // `}
+    // ${props=>props.location >= 21 && props.location <=30 &&`
+    //     position: relative; 
+    //     bottom:41px;
+    // `}
+    // ${props=>props.location >= 31 && props.location <=40 &&`
+    //     position: relative; 
+    //     bottom:38px;
+    // `}
 
 `
 
