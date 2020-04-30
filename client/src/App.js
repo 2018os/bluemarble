0726602b78f9fbcc47a5bc76c0101858f26121ed
김승엽 <seungyeop-kim@gimseung-yeobs-MacBook-Pro.local>
2020-05-01 01:47:37 +0900

    Except redux provider

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CountryList from './CountryList';
+
+const App = () => (
+  <div>
+    <CountryList />
+  </div>
+);
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './Root';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 //리덕스
@@ -15,9 +16,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
-ReactDOM.render(
-  <Provider store = {store}>
-    <Root />
-  </Provider>, document.getElementById('root')
-);
+// ReactDOM.render(
+//   <Provider store = {store}>
+//     <Root />
+//   </Provider>, document.getElementById('root')
+// );
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 serviceWorker.register();
